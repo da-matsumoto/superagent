d3d7270696551ae17c8b8b4939595efd61f08311
da-matsumoto <da.mats1990@gmail.com>
2018-04-17 12:50:43 +0900

    読み込み中を作成

--- sagent/src/App.js
+++ sagent/src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import request from 'superagent'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor (props) {
+    super(props)
+    this.state = {
+      items: null
+    }
+  }
+  componentWillMount () {
+    request.get('../public/fruits.json')
+      .accept('application/json')
+      .end((err, res) => {
+        this.loadedJSON(err, res)
+      })
+  }
+  loadedJSON (err, res) {
+    if (err) {
+      console.log('JSON読み込みエラー')
+      return
+    }
+    this.setState({
+      items: res.body
+    })
+  }
+  render () {
+    if (!this.state.items) {
+      return <div className='App'>現在読み込み中</div>
+    }
   }
 }
-
 export default App;
